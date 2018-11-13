e079b690d25df596f64c37cede133a9aeb179747
Joaquín <joaquin.aldunate@gmail.com>
2018-11-13 12:02:52 +0200

    customizing single pilot page, need replanning the classes hierarchy a bit

--- dist/template-parts-sections/single-content-pilot.php
+++ dist/template-parts-sections/single-content-pilot.php
@@ -35,51 +35,77 @@ This insert contains the page content part, to be inserted in the body of any pa
                 }
             ?>
         </span>
-    </div>
-    <div class="section-container section-post-container">
         <?php if(has_post_thumbnail()){ ?>
             <div <?php post_class('item-post-thumbnail-container')?>>
                     <?php the_post_thumbnail(); ?>
             </div>
         <?php } ?>
-        
-        
-        
-        <?php
-        //from a list of potential fields, display all those which are present as post body
-        //displayfields title => field_slug.
-        //TODO: create a way to customize the display titles of these fields. There should be an easy way.
-        //if not, I could use a consistent text transformation function, such as replacing underscore with spaces
-            $displayfields=Array(
-                "Overview"=>"overview",
-                "Description"=>"description",
-                "People"=>"people",
-                "Pedagogical methods"=>"pedagogical_methods_used",
-                "Tools used"=>"tools_used",
-                "Links and materials"=>"links_materials",
-                "Reflection"=>"reflection"
-            );
-
-            foreach($displayfields as $title => $fieldSlug){
-                if( get_field($fieldSlug) ){
-                    echo '<div class="item-'.$fieldSlug.'-field-container">';
-                    echo '  <h2 class="title">'.$title.'</h2>';
-                    echo '  <p class="content">';
-                    echo        get_field($fieldSlug);
-                    echo '  </p>';
-                    echo '</div>';
-                }
-            }
-        ?>
-        
-        <?php edit_post_link('Edit', '<span class="edit-link">', '</span>');?>
-        
     </div>
+    <div class="section-container section-post-container">
+        <div class="item-side-data-container">
+            <?php
+                //from a list of potential fields, display all those which are present as post body
+                //displayfields title => field_slug.
+                //TODO: create a way to customize the display titles of these fields. There should be an easy way.
+                //if not, I could use a consistent text transformation function, such as replacing underscore with spaces
+                $displayfields = array(
+                    "Pilot leader" => "people",
+                    "School" => "school",
+                    "Reach" => "reach",
+                    "Timeline" => "timeline",
+                );
+
+                foreach ($displayfields as $title => $fieldSlug) {
+                    if (get_field($fieldSlug)) {
+                        echo '<div class="item-' . $fieldSlug . '-field-container">';
+                        echo '  <h2 class="title">' . $title . '</h2>';
+                        echo '  <p class="content">';
+                        echo get_field($fieldSlug);
+                        echo '  </p>';
+                        echo '</div>';
+                    }
+                }
+                ?>
+        </div>
+        <div class="item-post-content-container">
+            <?php
+                //the same
+                $displayfields=Array(
+                    "Overview"=>"overview",
+                    "Description"=>"description",
+                    "People"=>"people",
+                    "Pedagogical methods"=>"pedagogical_methods_used",
+                    "Tools used"=>"tools_used",
+                    "Links and materials"=>"links_materials",
+                    "Reflection"=>"reflection"
+                );
+
+                foreach($displayfields as $title => $fieldSlug){
+                    if( get_field($fieldSlug) ){
+                        echo '<div class="item-'.$fieldSlug.'-field-container">';
+                        echo '  <h2 class="title">'.$title.'</h2>';
+                        echo '  <p class="content">';
+                        echo        get_field($fieldSlug);
+                        echo '  </p>';
+                        echo '</div>';
+                    }
+                }
+            ?>
+            
+            <?php edit_post_link('Edit', '<span class="edit-link">', '</span>');?>
+        </div>
+    </div>
+    <?php if(is_user_logged_in()){ ?>
+        <div class="section container section-dev-container">
+            <h2>Fields:</h2>
+            <textarea style="width:100%; height:300px">
+                <?php print_r( get_fields() ) ?>
+            </textarea>
+                
+            <h2>Post:</h2>
+            <textarea style="width:100%; height:300px">
+                <?php print_r( get_post() ) ?>
+            </textarea>
+        </div>
+    <?php } ?>
 <?php endwhile;?>
-<?php if(is_user_logged_in()){ ?>
-<div class="section container section-dev-container">
-    <textarea style="width:100%; height:300px">
-        <?php print_r( get_fields() ) ?>
-    </textarea>
-</div>
-<?php } ?>
